d88966101aecbe4d04523877a3b7ae1e16e3a2bf
Jean Christophe Petkovich <me@jcpetkovich.com>
2012-01-12 15:53:35 -0500

    cleanup to the xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -3,63 +3,53 @@
 --
 
 import XMonad
-import System.Exit
-import XMonad.Actions.UpdatePointer
+import System.Exit (ExitCode (ExitSuccess), exitWith)
+import System.IO (hPutStrLn)
+import XMonad.Actions.UpdatePointer (PointerPosition (Relative), updatePointer)
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.ManageHelpers
-import XMonad.Layout.NoBorders
-import System.IO
-
-import XMonad.Hooks.EwmhDesktops
-import XMonad.Hooks.SetWMName
-
+import XMonad.Hooks.DynamicLog (dynamicLogWithPP, dzenPP, ppOutput, ppCurrent, wrap, ppTitle, ppLayout, ppHidden)
+import XMonad.Hooks.ManageDocks (ToggleStruts (ToggleStruts), avoidStruts)
+import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
+import XMonad.Hooks.SetWMName (setWMName)
+import XMonad.Layout.NoBorders (noBorders)
 import qualified XMonad.StackSet as W
-import qualified Data.Map        as M
-import Data.List
-import Control.Monad
-import Data.Char
-import System.Environment
- 
+import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
+
+import qualified Data.Map as M
+
+------------------------------------------------------------------------
+-- Commands
+--
 dmenuCommand = "dmenu_run -nb '#242424' -nf '#D8BFD8'"
 
 dzenCommand = readFile "etc/dzen/dzencommand"
 
+emacsCommand = "emacsclient -c ~/org/whiteboard.org || (emacs --daemon && emacsclient -c ~/org/whiteboard.org)"
+
+starcraft = "wine ~/.wine/drive_c/Program\\ Files/StarCraft/StarCraft.exe"
+
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-jcKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
-    [ -- applications
-      ((modm, xK_Return          ), spawn $ XMonad.terminal conf                                               ) -- launch terminal
-    , ((modm, xK_v               ), windows W.swapMaster                                                       ) -- swap current and master
-    , ((modm, xK_p               ), spawn dmenuCommand                                                         ) -- launch krunner
-    , ((modm, xK_b               ), sendMessage ToggleStruts                                                   ) -- Toggle Struts
-    , ((modm, xK_f               ), spawn "firefox"                                                            ) -- launch firefox
-    , ((modm, xK_e               ), spawn "emacsclient -c ~/org/whiteboard.org || (emacs --daemon && emacsclient -c ~/org/whiteboard.org)" ) -- launch emacs
-    , ((modm .|. shiftMask, xK_s ), spawn "wine ~/.wine/drive_c/Program\\ Files/StarCraft/StarCraft.exe"       ) -- launch SC:BW
---    , ((modm, xK_d             ), spawn "konqueror --profile 'JC profile'"                                   ) -- launch konqueror
-    , ((modm, xK_c               ), kill                                                                       ) -- close window
-    -- , ((modm, xK_q               ), spawn "thunar"                                                             ) -- launch kde systemsettings
-    , ((modm .|. shiftMask, xK_l ), spawn "slock"                                                              ) -- lock
+mergeKeys c k = c `additionalKeysP` k c
+jckeys c = [ ("M-<Return>", spawn $ terminal c)   -- launch terminal
+           , ("M-v", windows W.swapMaster)        -- swap current and master
+           , ("M-p", spawn dmenuCommand)          -- launch dmenu
+           , ("M-b", sendMessage ToggleStruts)    -- Toggle Struts
+           , ("M-f", spawn "firefox")             -- launch firefox
+           , ("M-e", spawn emacsCommand)          -- launch emacs
+           , ("M-S-s", spawn starcraft)           -- launch SC:BW
+           , ("M-c", kill)                        -- close window
+           , ("M-S-l", spawn "slock")             -- lock
+           , ("M-8", spawn "mpc toggle")          -- pause/play
+           , ("M-7", spawn "mpc prev")            -- previous song
+           , ("M-0", spawn "mpc next")            -- next song
+           , ("M--", spawn "mpc volume -5")       -- volume - 5
+           , ("M-=", spawn "mpc volume +5")       -- volume + 5
+           , ("M-S-z", io (exitWith ExitSuccess)) -- exit xmonad
+           ]
 
-    -- music
-    -- , ((modm, xK_8            ), spawn "qdbus org.kde.amarok /Player org.freedesktop.MediaPlayer.PlayPause" ) -- pause/play
-    -- , ((modm, xK_7            ), spawn "qdbus org.kde.amarok /Player org.freedesktop.MediaPlayer.Prev"      ) -- previous song
-    -- , ((modm, xK_0            ), spawn "qdbus org.kde.amarok /Player org.freedesktop.MediaPlayer.Next"      ) -- next song
-    , ((modm, xK_8               ), spawn "mpc toggle"                                                         ) -- pause/play
-    , ((modm, xK_7               ), spawn "mpc prev"                                                           ) -- previous song
-    , ((modm, xK_0               ), spawn "mpc next"                                                           ) -- next song
-    , ((modm, xK_minus           ), spawn "mpc volume -5"                                                      ) -- volume - 5
-    , ((modm, xK_equal           ), spawn "mpc volume +5"                                                      ) -- volume + 5
-    , ((modm .|. shiftMask, xK_z ), io (exitWith ExitSuccess)                                                  ) -- exit xmonad
-    ]
 
-    ++
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
-        | (key, sc) <- zip [xK_w, xK_q, xK_d] [0..]
-        , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-    
 ------------------------------------------------------------------------
 -- Layout Hook:
 --
@@ -91,23 +81,25 @@ main = do
        , modMask            = mod4Mask
        , normalBorderColor  = "#dddddd"
        , focusedBorderColor = "#ff0000"
+                              
+       -----------------------------------------------------------------
+       -- Screen related keys
+       --
+       , keys = \c -> M.fromList [((m .|. modMask c, key), screenWorkspace sc >>= flip whenJust (windows . f))
+                                  | (key, sc) <- zip [xK_q, xK_w, xK_d] [0..]
+                                 , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+                `M.union` keys defaultConfig c 
 
        -----------------------------------------------------------------
        -- Workspaces:
        --
        , workspaces         = ["term","net","emacs","docs","media","misc"] 
- 
-       -----------------------------------------------------------------
-       -- Key Definitions moved elsewhere for cleanliness:
-       --
-       , keys               = \c -> jcKeys c `M.union` 
-                              keys defaultConfig c 
-
+                              
        -----------------------------------------------------------------
        -- Hooks:
        --
        , startupHook        = startupHook defaultConfig >> setWMName "LG3D"
-       , layoutHook         = avoidStruts $ jcLayoutHook
+       , layoutHook         = avoidStruts jcLayoutHook
        , logHook            = dynamicLogWithPP dzenPP 
                               { 
                                 ppOutput = hPutStrLn xmproc
@@ -115,28 +107,24 @@ main = do
                               , ppTitle = wrap "^fg(#9ACD32)^bg(#333333)" " ^bg()^fg()"
                               , ppLayout = wrap "^fg(#D8BFD8)^bg(#333333) : " " : ^bg()^fg()"
                               , ppHidden = wrap "^fg(#D8BFD8)^bg(#333333) " "^bg()^fg()"
-                              --   ppCurrent = xmobarColor "white" "" 
-                              --   -- . wrap "[" "]",
-                              -- , ppTitle = xmobarColor "#9ACD32" "" 
                               } >> updatePointer (Relative 0.99 0.99) --Nearest
-
+       
        -- Rules for applications:
        , manageHook         = manageHook defaultConfig <+> myManageHook
-       }
+       } `mergeKeys` jckeys 
+
        where
          myManageHook = composeAll 
-                              [ className =? "MPlayer"            --> doFloat
-                              , className =? "Gimp"               --> doFloat
-                              , className =? "KMenu"              --> doFloat
-                              , className =? "Amarok"             --> doFloat
-                              , title =? "Brood War"              --> doIgnore
-                              , className =? "trayer"              --> doIgnore
-                              , className =? "feh"                --> doFloat
-                              , className =? "Firefox"            --> doF(W.shift "net")
-                              -- , className =? "Emacs"           --> doF(W.shift "emacs")
-                              , className =? "OpenOffice.org 3.0" --> doF(W.shift "docs")
-                              , className =? "Pidgin"             --> doF(W.shift "term")
-                              -- , className =? "Evince"             --> doF(W.shift "docs")
-                              , isFullscreen --> doFullFloat]
+                        [ className =? "MPlayer"            --> doFloat
+                        , className =? "Gimp"               --> doFloat
+                        , className =? "KMenu"              --> doFloat
+                        , className =? "Amarok"             --> doFloat
+                        , title =? "Brood War"              --> doIgnore
+                        , className =? "trayer"             --> doIgnore
+                        , className =? "feh"                --> doFloat
+                        , className =? "Firefox"            --> doF(W.shift "net")
+                        , className =? "OpenOffice.org 3.0" --> doF(W.shift "docs")
+                        , className =? "Pidgin"             --> doF(W.shift "term")
+                        , isFullscreen --> doFullFloat ]
 
 
