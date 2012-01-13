f49c16d1634d4dc159bf98de6ab35d89eed2d18f
Jean Christophe Petkovich <me@jcpetkovich.com>
2012-01-12 21:11:38 -0500

    more changes to xmonad config

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,7 +7,7 @@ import System.Exit (ExitCode (ExitSuccess), exitWith)
 import System.IO (hPutStrLn)
 import XMonad.Actions.UpdatePointer (PointerPosition (Relative), updatePointer)
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Hooks.DynamicLog (dynamicLogWithPP, dzenPP, ppOutput, ppCurrent, wrap, ppTitle, ppLayout, ppHidden)
+import XMonad.Hooks.DynamicLog (PP(..), defaultPP, dynamicLogWithPP, dzenPP, ppOutput, ppCurrent, wrap, ppTitle, ppLayout, ppHidden)
 import XMonad.Hooks.ManageDocks (ToggleStruts (ToggleStruts), avoidStruts)
 import XMonad.Hooks.ManageHelpers (isFullscreen, doFullFloat)
 import XMonad.Hooks.SetWMName (setWMName)
@@ -16,6 +16,8 @@ import qualified XMonad.StackSet as W
 import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
 
 import qualified Data.Map as M
+import Control.Arrow ((&&&))
+import Data.Char (toLower)
 
 ------------------------------------------------------------------------
 -- Commands
@@ -71,7 +73,6 @@ jcLayoutHook = tiled ||| Mirror tiled ||| noBorders Full
 -- Main:
 --
 main = do
-  xmproc <- spawnPipe =<< dzenCommand
   xmonad $ defaultConfig
        { 
        -----------------------------------------------------------------
@@ -83,34 +84,27 @@ main = do
        , focusedBorderColor = "#ff0000"
                               
        -----------------------------------------------------------------
-       -- Screen related keys
-       --
-       , keys = \c -> M.fromList [((m .|. modMask c, key), screenWorkspace sc >>= flip whenJust (windows . f))
-                                  | (key, sc) <- zip [xK_q, xK_w, xK_d] [0..]
-                                 , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
-                `M.union` keys defaultConfig c 
-
-       -----------------------------------------------------------------
        -- Workspaces:
        --
-       , workspaces         = ["term","net","emacs","docs","media","misc"] 
+       , workspaces         = ["term","web","emacs","doc","vid","misc"] 
                               
        -----------------------------------------------------------------
        -- Hooks:
        --
        , startupHook        = startupHook defaultConfig >> setWMName "LG3D"
        , layoutHook         = avoidStruts jcLayoutHook
-       , logHook            = dynamicLogWithPP dzenPP 
-                              { 
-                                ppOutput = hPutStrLn xmproc
-                              , ppCurrent = wrap "^fg(white)^bg(#333333) " "^bg()^fg()"
-                              , ppTitle = wrap "^fg(#9ACD32)^bg(#333333)" " ^bg()^fg()"
-                              , ppLayout = wrap "^fg(#D8BFD8)^bg(#333333) : " " : ^bg()^fg()"
-                              , ppHidden = wrap "^fg(#D8BFD8)^bg(#333333) " "^bg()^fg()"
-                              } >> updatePointer (Relative 0.99 0.99) --Nearest
+       , logHook            =  myLogHook
        
        -- Rules for applications:
        , manageHook         = manageHook defaultConfig <+> myManageHook
+
+       -----------------------------------------------------------------
+       -- Screen related keys
+       --
+       , keys = \c -> M.fromList [((m .|. modMask c, key), screenWorkspace sc >>= flip whenJust (windows . f))
+                                  | (key, sc) <- zip [xK_q, xK_w, xK_d] [0..]
+                                 , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+                `M.union` keys defaultConfig c 
        } `mergeKeys` jckeys 
 
        where
@@ -128,3 +122,50 @@ main = do
                         , isFullscreen --> doFullFloat ]
 
 
+myLogHook = do 
+  xmproc <- io $ spawnPipe =<< dzenCommand
+  ws <- gets windowset
+  let sl = if null $ W.visible ws then [] else W.screens ws
+  let m = map ((W.tag . W.workspace) &&& W.screen) sl
+  dynamicLogWithPP $ defaultPP
+                       { ppCurrent = myPPCurrent m
+                       , ppVisible = myPPVisible m
+                       , ppHidden = myPPHidden
+                       , ppHiddenNoWindows = myPPHidden . (++ "-empty")
+                       , ppUrgent = const ""
+                       , ppSep = ""
+                       , ppWsSep = ""
+                       , ppTitle = const ""
+                       , ppLayout = myPPLayout
+                       , ppOrder = id
+                       , ppOutput = hPutStrLn xmproc
+                       }
+      where
+        myPPCurrent m t = activeWorkspace t ++ iconScreen (lookup t m)
+        myPPVisible m t = inactiveWorkspace t ++ iconScreen (lookup t m)
+        myPPHidden = (++ screenPad) . inactiveWorkspace
+        myPPLayout = wrap "-^p(+6)" "^p(+7)" . iconLayout
+
+iconWrap :: String -> String
+iconWrap = wrap "^i(/home/jcp/etc/dzen/pixmaps/" ".xpm)"
+
+bracketPad :: String
+bracketPad = "^p(+4)"
+
+screenPad :: String
+screenPad = "^p(+4)"
+
+activeWorkspace :: String -> String
+activeWorkspace = wrap lb rb . iconWrap
+  where lb = iconWrap "left-bracket"
+        rb = iconWrap "right-bracket"
+
+inactiveWorkspace :: String -> String
+inactiveWorkspace = wrap bracketPad bracketPad . iconWrap
+
+iconLayout :: String -> String
+iconLayout = iconWrap . ("layout-" ++) . map ((\c -> if c == ' ' then '-' else c) . toLower)
+
+iconScreen :: Maybe ScreenId -> String
+iconScreen Nothing = "^p(+4)"
+iconScreen (Just (S s)) = iconWrap $ ("screen-" ++) . show $ s + 1
