34367a45e4850df176bc60fba1c740cae23a9da4
Jean-Christophe Petkovich <me@jcpetkovich.com>
2013-07-16 23:45:04 -0400

    Added another layout for making new ebuilds.

---
+++ tmux/layouts/overlay-personal.session.sh
@@ -0,0 +1,18 @@
+# Set a custom session root path. Default is `$HOME`.
+# Must be called before `initialize_session`.
+session_root "~/jc-public/projects/overlay-eyolfson"
+
+# Create session with specified name if it does not already exist. If no
+# argument is given, session name will be based on layout file name.
+if initialize_session "overlay-personal"; then
+
+  new_window "emacs"
+  run_cmd "new-ebuild.sh"
+
+  new_window "ebuild"
+  select_window "emacs"
+
+fi
+
+# Finalize session creation and switch/attach to it.
+finalize_and_go_to_session
