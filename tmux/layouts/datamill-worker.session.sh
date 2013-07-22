cf9d6e79a70215b658892dd766efcf72d3fd5141
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-07-22 18:04:01 -0400

    Changed worker session to match new worker dir.

--- tmux/layouts/datamill-worker.session.sh
+++ tmux/layouts/datamill-worker.session.sh
@@ -1,6 +1,6 @@
 # Set a custom session root path. Default is `$HOME`.
 # Must be called before `initialize_session`.
-session_root "~/jc-public/projects/eval-lab/new-new-worker"
+session_root "~/jc-public/projects/eval-lab/worker"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
