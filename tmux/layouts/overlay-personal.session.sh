94b224a8161dc4373954b8e3a5eb56e66fac8cad
Jean Christophe Petkovich <me@jcpetkovich.com>
2013-08-06 17:12:23 -0400

    Updated overlay layout.

--- tmux/layouts/overlay-personal.session.sh
+++ tmux/layouts/overlay-personal.session.sh
@@ -4,7 +4,7 @@ session_root "~/jc-public/projects/overlay-eyolfson"
 
 # Create session with specified name if it does not already exist. If no
 # argument is given, session name will be based on layout file name.
-if initialize_session "overlay-personal"; then
+if initialize_session "overlay"; then
 
   new_window "emacs"
   run_cmd "new-ebuild.sh"
