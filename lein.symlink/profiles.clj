a138b5670fdba20f3929891114926ea171b39af9
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2014-11-19 08:09:18 -0500

    Update nrepl

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,3 +1,3 @@
 {:user {:plugins [[lein-oneoff "0.3.1"]
                   [lein-exec "0.3.4"]
-                  [cider/cider-nrepl "0.7.0"]]}}
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
