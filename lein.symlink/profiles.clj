0afab0027f53100f713270238d9c516b80c9b048
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2015-01-14 16:51:48 -0500

    Remove default jvm opts for leiningen

--- lein.symlink/profiles.clj
+++ lein.symlink/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-oneoff "0.3.1"]
                   [lein-exec "0.3.4"]
-                  [cider/cider-nrepl "0.8.2-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+        :jvm-opts []}}
