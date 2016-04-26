480363c0206157b0d7638f764112bf50de032814
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2016-04-26 16:23:52 -0400

    Commit work/home scripts.

---
+++ bin.symlink/home.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+xrandr --output HDMI-0 --off --output eDP-0 --auto
+colemak3
---
+++ bin.symlink/work.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+xrandr --output HDMI-0 --auto --output eDP-0 --off
+ergodox
