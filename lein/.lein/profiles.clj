a7263747b7797106c42ff17ba93a482ba0461d1f
Jean-Christophe Petkovich <jcpetkovich@gmail.com>
2019-04-16 11:44:29 -0400

    Long past due updates.

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,4 +1,2 @@
-{:user {:plugins [[lein-oneoff "0.3.1"]
-                  [lein-exec "0.3.4"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+{:user {:plugins [[cider/cider-nrepl "0.21.1"]]
         :jvm-opts []}}
